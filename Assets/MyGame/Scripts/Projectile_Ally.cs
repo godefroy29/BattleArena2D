87ae680847c0858a9f236425f5ee0fa090123dd1
godefroy29 <godefroy.29@gmail.com>
2018-01-31 23:25:00 +0100

    Fix BulletAlly Collider
    With Layer

--- Assets/MyGame/Scripts/Projectile_Ally.cs
+++ Assets/MyGame/Scripts/Projectile_Ally.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile_Ally : MonoBehaviour {
-     
+
     void OnCollisionEnter (Collision col)
     {
         switch (col.gameObject.tag)
         {
+            case "bulletAlly":
+                Physics.IgnoreCollision(col.collider, GetComponent<Collider>());
+                break;
+    
             case "enemy":
                 Destroy(col.gameObject);
                 Destroy(this.gameObject);
