918ae473e29522eaee75177d5f7b38bd94ae33ea
godefroy29 <godefroy.29@gmail.com>
2018-01-31 22:28:54 +0100

    Correct Bullet Ally
    Bug - collider bulletAlly/player

--- Assets/MyGame/Scripts/PlayerControl.cs
+++ Assets/MyGame/Scripts/PlayerControl.cs
@@ -6,62 +6,56 @@ public class PlayerControl : MonoBehaviour {
 
 
     public GameObject bullet;
-    
-	public float moveSpeed;
 	public float bulletSpeed;
+	public float moveSpeed;
     public float addX;
     public float fireDelayMillisec;
     private System.DateTime lastFiredBullet;
-	private KeyCode RightKey = KeyCode.D; //shortcut
-	private KeyCode LeftKey = KeyCode.Q; //shortcut
-	private KeyCode UpKey = KeyCode.Z; //shortcut
-    private KeyCode FireLeftKey = KeyCode.LeftArrow;
-    private KeyCode FireRightKey = KeyCode.RightArrow;
-    private bool freezeRotation = true;
 
 
     void Start()
     {
         GetComponent<Rigidbody>().freezeRotation = true;
     }
-	// Update is called once per frame
-	void Update () {
-		Vector2 movement = Vector2.zero;
+
+    void fire(int directionX)
+    {
+        if((System.DateTime.Now - lastFiredBullet).TotalMilliseconds >= fireDelayMillisec)
+        {
+            GameObject bulletInstance = Instantiate(bullet, new Vector3(transform.position.x + directionX * addX, transform.position.y,0), Quaternion.Euler(new Vector3(0, 0, 0)));
+            bulletInstance.GetComponent<Rigidbody>().velocity = new Vector3(directionX * 1,0,0) * bulletSpeed;
+            Physics2D.IgnoreCollision(bulletInstance.GetComponent<Collider2D>(),  GetComponent<Collider2D>());
+            lastFiredBullet = System.DateTime.Now;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 movement = Vector2.zero;
  
-        if(Input.GetKey(LeftKey))
+        if(Input.GetKey(KeyCode.Q))
         {
             movement.x = (transform.right*Time.deltaTime*-moveSpeed).x;
         }
-        if(Input.GetKey(RightKey))
+        if(Input.GetKey(KeyCode.D))
         {
             movement.x = (transform.right*Time.deltaTime*moveSpeed).x;
         }
-        if(Input.GetKey(UpKey))
+        if(Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Z))
         {
             movement.y = (transform.up*Time.deltaTime*moveSpeed).y;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            fire(-1);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            fire(1);
         }
  
         movement = movement + (Vector2)(transform.position);
  
         GetComponent<Rigidbody>().MovePosition(movement);
-
-            
-        if((System.DateTime.Now - lastFiredBullet).TotalMilliseconds >= fireDelayMillisec)
-        {
-            if (Input.GetKey(FireLeftKey))
-            {
-                GameObject bulletInstance = Instantiate(bullet, new Vector3(transform.position.x - addX, transform.position.y,0), Quaternion.Euler(new Vector3(0, 0, 0)));
-                bulletInstance.GetComponent<Rigidbody>().velocity = new Vector3(-1,0,0) * bulletSpeed;
-                Physics2D.IgnoreCollision(bulletInstance.GetComponent<Collider2D>(),  GetComponent<Collider2D>());
-            }
-            if (Input.GetKey(FireRightKey))
-            {
-                GameObject bulletInstance = Instantiate(bullet, new Vector3(transform.position.x + addX, transform.position.y,0), Quaternion.Euler(new Vector3(0, 0, 0)));
-                bulletInstance.GetComponent<Rigidbody>().velocity = new Vector3(1,0,0) * bulletSpeed;
-                Physics2D.IgnoreCollision(bulletInstance.GetComponent<Collider2D>(),  GetComponent<Collider2D>());
-            }
-
-            lastFiredBullet = System.DateTime.Now;
-       }
-    }	
+    }
 }
