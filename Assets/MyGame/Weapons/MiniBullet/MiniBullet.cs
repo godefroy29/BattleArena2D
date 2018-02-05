b5c00db003397abcb0706eebf3b41cf8e80240bc
godefroy29 <godefroy.29@gmail.com>
2018-02-05 21:31:48 +0100

    test with another bullet

---
+++ Assets/MyGame/Weapons/MiniBullet/MiniBullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniBullet : Weapon {
+
+	void OnDestroy() {
+        // GameObject smokePuff = Instantiate(smoke, transform.position, transform.rotation) as GameObject;
+		// ParticleSystem parts = smokePuff.GetComponent<ParticleSystem>();
+		// float totalDuration = parts.main.duration + parts.startLifetime;
+		// Destroy(smokePuff, totalDuration);
+    }
+}
